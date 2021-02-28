4c1eecc3876561bc091fb69775b8dd2dc4d94ec6
yeiipi <juanpab.sierra@urosario.edu.co>
2021-02-28 11:33:50 -0500

    Funciones documentadas
    issue no: UD010

--- f_julia_set.m
+++ f_julia_set.m
@@ -1,4 +1,21 @@
 function W = f_julia_set(res,col,n,c)
+%f_julia_set  | Función según la resolución res, la cantidad de iteraciones
+%               col, y la constante c genera una matriz con valores entre
+%               [1,0] cumpliendo las propiedades del set de Julia.
+%               ===========================================================
+%               PARÁMETROS:
+%                  - res : resolución de la imágen, cantidad tamañod de la
+%                          matriz Z.
+%                  - col : Cantidad de iteraciones de Z.
+%                  -   n : Exponente de de Z.
+%                  -   c : Constante que se le suma a cada iteración de Z.
+%               ===========================================================
+%               RETORNA:
+%                  - W : Matriz con valores de [1,0] que cumplen las
+%                        propiedades del set de Julia.
+
+
+
 
 l=1.5;
 
